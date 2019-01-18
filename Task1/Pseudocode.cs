951f847d6243c0d277b95c7e0f02149de26f83c6
wes webbert <weswebbert@gmail.com>
2019-01-17 20:58:25 -0700

    Final

---
+++ Task1/Pseudocode.cs
@@ -0,0 +1,14 @@
+//setup
+     var miles;
+     var gallons;   
+     Single Division = 0;
+    
+//input
+    "How many miles traveled?" GET miles
+    "How many gallons used to fill the tank?" GET gallons
+
+//processing
+    Division = miles / gallons
+
+//output
+    PUT "Division of " + miles + " + " + gallons + " equals a MPG of " + Division);
--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -6,7 +6,27 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //setup
+
+            Single Division = 0;
+            
+            //input
+
+            Console.WriteLine("what is the distance traveled in miles?");
+            var miles = Single.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many gallons used to fill the tank?");
+            var gallons = Single.Parse(Console.ReadLine());
+
+            //processing
+            Division = miles / gallons;
+            
+
+            //output
+
+            Console.WriteLine("Division of " + miles + " + " + gallons + " equals a MPG of " + Division );
+            Console.ReadLine();
+        
         }
     }
 }
